a9d2c228406c21cdfdce19cb27fc2fb784b6ff40
Soekasir <soekasir@gmail.com>
2022-10-13 04:50:33 +0700

    opt: delete ioc, add container

--- src/index.tsx
+++ src/index.tsx
@@ -4,18 +4,16 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import Router from "./routers";
 import "./styles/global.css";
-import { GlobalContainer } from "./utils/IoC";
+import { SingletonContainer } from "./utils/container";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 
 root.render(
-  // <React.StrictMode>
-    <GlobalContainer>
-      <BrowserRouter>
-        <Router />
-      </BrowserRouter>
-    </GlobalContainer>
-  // </React.StrictMode>
+  <SingletonContainer>
+    <BrowserRouter>
+      <Router />
+    </BrowserRouter>
+  </SingletonContainer>
 );
